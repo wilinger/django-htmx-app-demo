1a0595a097f7f0468a6de71cfe4dc3f9163f3874
wilinger <>
2022-02-22 20:51:11 -0800

    add require_http_methods

--- src/notecards/views.py
+++ src/notecards/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.http import require_http_methods
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
@@ -6,7 +7,7 @@ from django.shortcuts import render
 from notecards.models import Notecard
 from .forms import NoteForm
 
-
+@require_http_methods(["GET"])
 def index(request):
     return render(request, 'notecards/index.html')
 
@@ -18,6 +19,7 @@ class NotecardDetailView(DetailView):
     model = Notecard
     template_name = 'notecards/partials/notecard_detail.html'
 
+@require_http_methods(["GET", "POST"])
 def notecard_create(request):
     if request.method == "POST":
         form = NoteForm(request.POST)
@@ -33,6 +35,7 @@ def notecard_create(request):
     }
     return render(request, template_name, context)
 
+@require_http_methods(["GET", "POST"])
 def notecard_update(request, pk):
     notecard = get_object_or_404(Notecard, pk=pk)
     if request.method == "POST":
@@ -49,6 +52,7 @@ def notecard_update(request, pk):
     }
     return render(request, template_name, context)
 
+@require_http_methods(["DELETE"])
 def notecard_delete(request, pk):
     if request.method == "DELETE":
         notecard = get_object_or_404(Notecard, pk=pk)
