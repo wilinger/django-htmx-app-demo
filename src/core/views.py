0327f3da573bcf08a8876dae92f90f359b48e954
Will Wong <17818801+wilinger@users.noreply.github.com>
2022-02-23 00:11:43 -0800

    update dockerfile (#7)
    * update dockerfile
    
    * change msg
    
    Co-authored-by: wilinger <>

--- src/core/views.py
+++ src/core/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET"])
 def index_view(request):
-    msg = "hello world"
+    msg = "Welcome."
 
     context = {
         "msg": msg
